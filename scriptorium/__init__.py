ec6fa7531d8f43b0a4e90b67f41f539ef777eb68
andy-zen-dev <andyzendev@gmail.com>
2026-09-12 17:00:35 +0300

    api: export deterministic metric primitives

--- scriptorium/__init__.py
+++ scriptorium/__init__.py
@@ -1,11 +1,21 @@
 """Scriptorium analysis primitives."""
 
+from .metrics import (
+    METRIC_PROFILE,
+    PUNCTUATION_PROFILE,
+    analyze_deterministic_metrics,
+    punctuation_counts,
+)
 from .text import NORMALIZATION_PROFILE, TextSpan, normalize_text, sentence_spans, word_tokens
 
 __all__ = [
+    "METRIC_PROFILE",
     "NORMALIZATION_PROFILE",
+    "PUNCTUATION_PROFILE",
     "TextSpan",
+    "analyze_deterministic_metrics",
     "normalize_text",
+    "punctuation_counts",
     "sentence_spans",
     "word_tokens",
 ]
